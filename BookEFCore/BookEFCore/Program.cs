93f82a4bf46e9bd87d26a893040c0a6635724c34
JonathanFL <j.lydemann@gmail.com>
2019-03-04 10:58:27 +0100

    Init commit.

---
+++ BookEFCore/BookEFCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
